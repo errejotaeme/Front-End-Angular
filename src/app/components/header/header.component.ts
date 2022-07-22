73b15fd780c80dd7e96e314b999eeec1dd46a944
Juan Manuel Rodríguez Miguel <rodriguezjuanma89@outlook.com>
2022-07-22 19:49:51 -0300

    Cambio imagen logo AP segun tema

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,14 +13,19 @@ export class HeaderComponent implements OnInit {
   }
 
   tema: boolean = false;
+  clase: string = "";
   alternarTemaColor(){
      if(this.tema == false){
     this.renderer.addClass(document.body, 'tema-oscuro');
+    this.clase = "img-invertida";
      }else{
       this.renderer.removeClass(document.body, 'tema-oscuro');
+      this.clase = "";
      }
     this.tema = !this.tema;
     }
+
+
 
 
 
