96e05f960c938f7d96093c988f8bf8081569b73d
Juan Manuel Rodríguez Miguel <rodriguezjuanma89@outlook.com>
2022-07-23 18:45:28 -0300

    Instalacion correcta de iconos y modificaciones de estilo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { BotonComponent } from './components/boton/boton.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CompletoComponent } from './components/completo/completo.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -30,7 +31,8 @@ import { CompletoComponent } from './components/completo/completo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
