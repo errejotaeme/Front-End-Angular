e9a22a3f01413f8973c66617b8a032e603ee7a3e
Juan Manuel Rodríguez Miguel <rodriguezjuanma89@outlook.com>
2022-08-17 13:25:13 -0300

    prueba modificacion header por cors

--- src/app/servicios/autenticacion.service.ts
+++ src/app/servicios/autenticacion.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable, map } from 'rxjs';
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type':'application/json'}),
+  headers: new HttpHeaders({'Content-Type':'text/plain'}),
   responseType: 'json' as const
 }
 
